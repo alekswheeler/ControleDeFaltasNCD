2abf5aa63e623faa20f9c62e4d5dc4679b0595e0
angelomf <angelomfrechiani@gmail.com>
2023-12-02 18:12:30 -0300

    feat: implementa dependências restantes entre turma, aluno e presença

--- Domain/Presenca.cs
+++ Domain/Presenca.cs
@@ -1,11 +1,13 @@
 // Objeto de Domínio - Presença
 public class Presenca
 {
-    Presenca(DateOnly data, string registro)
+    Presenca(DateOnly data, string registro, Aluno[] alunos)
     {
         Data = data;
         Registro = registro;
+        Alunos = alunos;
     }
     public DateOnly Data { get; set; }
     public string Registro { get; set; }
+    public Aluno[] Alunos { get; set; }
 }
