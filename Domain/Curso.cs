3cdf9a7fd21acd6fae14ed418e2b712af95e48f9
alekswheeler <alexskntks@outlook.com>
2023-12-13 14:02:55 -0300

    Uodate classes to Mongo

--- Domain/Curso.cs
+++ Domain/Curso.cs
@@ -1,4 +1,10 @@
 // Objeto de Domínio - Curso
+namespace ControleDeFaltasNCD.Domain;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+[BsonIgnoreExtraElements]
 public class Curso
 {
     Curso(string nome, string descricao, string cargahoraria, string codigo)
@@ -8,6 +14,8 @@ public class Curso
         CargaHoraria = cargahoraria;
         Codigo = codigo;
     }
+
+    public ObjectId Id { get; set; }
     public string Nome { get; set; }
     public string Descricao { get; set; }
     public string CargaHoraria { get; set; }
